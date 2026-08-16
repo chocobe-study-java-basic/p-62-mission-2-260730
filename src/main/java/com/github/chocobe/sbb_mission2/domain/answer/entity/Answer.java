a1f42132c338c00812a26df16de4b515c519c572
Chocobe <kyw05171@gmail.com>
2026-08-16 18:01:49 +0900

    [복습 2-04] 엔티티로 테이블 매핑하기

---
+++ src/main/java/com/github/chocobe/sbb_mission2/domain/answer/entity/Answer.java
@@ -0,0 +1,25 @@
+package com.github.chocobe.sbb_mission2.domain.answer.entity;
+
+import com.github.chocobe.sbb_mission2.domain.question.entity.Question;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+public class Answer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @ManyToOne
+    private Question question;
+
+}
---
+++ src/main/java/com/github/chocobe/sbb_mission2/domain/question/entity/Question.java
@@ -0,0 +1,37 @@
+package com.github.chocobe.sbb_mission2.domain.question.entity;
+
+import com.github.chocobe.sbb_mission2.domain.answer.entity.Answer;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Getter
+public class Question {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 200)
+    private String subject;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @OneToMany(
+            mappedBy = "question",
+            fetch = FetchType.LAZY,
+            cascade = {
+                    CascadeType.PERSIST,
+                    CascadeType.REMOVE
+            },
+            orphanRemoval = true
+    )
+    private List<Answer> answerList;
+
+}
